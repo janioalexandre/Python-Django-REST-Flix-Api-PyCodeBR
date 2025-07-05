9d4d253945898eb4692e389bd7b39dd404a4f0e5
Janio Alexandre Ribeiro <janioalexandre@gmail.com>
2025-07-05 16:09:26 -0300

    Add validation to movie serializer

--- movies/serializers.py
+++ movies/serializers.py
@@ -1,8 +1,20 @@
 from rest_framework import serializers
 from movies.models import Movie
 
+
 class MovieSerializer(serializers.ModelSerializer):
     
     class Meta:
         model = Movie
         fields = ['id', 'title', 'genre', 'release_date', 'actors', 'resume']
+
+    def validate_release_date(self, value):
+        if value.year < 1990:
+            raise serializers.ValidationError("A data de lançamento não pode ser anterior a 1990.")
+        return value
+    
+    def validate_resume(self, value):
+        if len(value) > 200:
+            raise serializers.ValidationError("O resumo não deve ser maior que 200 caracteres.")
+        return value
+    
